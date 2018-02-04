44b88ee664737ab81b4619da232869c2816f278b
fengjiyuan <583280516@qq.com>
2018-02-04 18:58:19 +0800

    遗漏文件提交
    1. 增加 ReleaseAssetOnDestroy 组件可以用来自动回收场景对象的资源，用法参考 AssetsTest
    2. 增加 Assets 资源回收逻辑，修改之前预制件中静态引用的贴图，在销毁并卸载预制件后，在Profiler 中没有正常回收的问题
    3. 优化编码，尽可能把编辑器相关逻辑和 Runtime 逻辑分开，方便后续构建 dll，gendll.sh 可以用来在 mac 下生成 xasset 的 dll

---
+++ Assets/XAsset/ReleaseAssetOnDestroy.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+namespace XAsset
+{
+    public class ReleaseAssetOnDestroy : MonoBehaviour
+    { 
+        public Asset asset;
+
+        public static ReleaseAssetOnDestroy Register(GameObject go, Asset asset)
+        {
+            ReleaseAssetOnDestroy component = go.GetComponent<ReleaseAssetOnDestroy>();
+            if (component == null)
+            {
+                component = go.AddComponent<ReleaseAssetOnDestroy>();
+            }
+            component.asset = asset; 
+            return component;
+        }
+
+        private void OnDestroy()
+        {
+            if (asset != null)
+            {
+                asset.Release();
+                asset = null;
+            } 
+        }
+    }
+}
